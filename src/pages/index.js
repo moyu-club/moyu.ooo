3c8e7b80a0fa05a2ac634e56020822a16348d893
Rui Chen <rui@chenrui.dev>
2021-04-03 13:25:54 -0400

    update moyu logo and file layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -56,8 +56,8 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Built by{' '}
+          <img src="/moyu.svg" alt="Moyu Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
