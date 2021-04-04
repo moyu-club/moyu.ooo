f67b952ea82d4fe7e678d44c5fa356231987466d
Rui Chen <rui@chenrui.dev>
2021-04-04 17:15:26 -0400

    update resource layout for pwa
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,8 +6,12 @@ export default function Page() {
     <div className={styles.container}>
       <Head>
         <title>Moyu</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/site.webmanifest"></link>
+
+        <link rel='apple-touch-icon' sizes='180x180' href='/static/icons/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/static/icons/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/static/icons/favicon-16x16.png' />
+        <link rel="manifest" href="/static/site.webmanifest" />
+        <link rel="shortcut icon" href="/static/icons/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
