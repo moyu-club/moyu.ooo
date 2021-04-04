debafe082a11c69cd7524727095f090bd7e6cc6a
Rui Chen <rui@chenrui.dev>
2021-04-03 21:56:07 -0400

    remove cache-control headers
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- next.config.js
+++ next.config.js
@@ -1,18 +1,3 @@
 module.exports = {
   trailingSlash: true,
-  async headers() {
-    return [
-      {
-        // This works, and returns appropriate Response headers:
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value:
-              'public, max-age=180, s-maxage=180, stale-while-revalidate=180',
-          },
-        ],
-      },
-    ]
-  },
 }
