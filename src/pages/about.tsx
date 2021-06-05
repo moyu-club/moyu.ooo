376315bedb86f58597b0a23b9b18e2c6356598c9
Rui Chen <rui@chenrui.dev>
2021-06-05 02:50:18 -0400

    remove join link
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,9 +7,23 @@ export default function Page() {
       <Head>
         <title>Moyu</title>
 
-        <link rel='apple-touch-icon' sizes='180x180' href='/static/icons/apple-touch-icon.png' />
-        <link rel='icon' type='image/png' sizes='32x32' href='/static/icons/favicon-32x32.png' />
-        <link rel='icon' type='image/png' sizes='16x16' href='/static/icons/favicon-16x16.png' />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/static/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/static/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/static/icons/favicon-16x16.png"
+        />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="shortcut icon" href="/static/icons/favicon.ico" />
       </Head>
@@ -19,14 +33,8 @@ export default function Page() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://join.moyu.ooo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Built with by
-          <img src="/moyu.svg" alt="Moyu Logo" className={styles.logo} />
-        </a>
+        Built with by
+        <img src="/moyu.svg" alt="Moyu Logo" className={styles.logo} />
       </footer>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,17 +7,29 @@ export default function Home() {
       <Head>
         <title>Moyu</title>
 
-        <link rel='apple-touch-icon' sizes='180x180' href='/static/icons/apple-touch-icon.png' />
-        <link rel='icon' type='image/png' sizes='32x32' href='/static/icons/favicon-32x32.png' />
-        <link rel='icon' type='image/png' sizes='16x16' href='/static/icons/favicon-16x16.png' />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/static/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/static/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/static/icons/favicon-16x16.png"
+        />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="shortcut icon" href="/static/icons/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://join.moyu.ooo">Moyu Club</a>!
-        </h1>
+        <h1 className={styles.title}>Welcome to Moyu Club!</h1>
 
         <p className={styles.description}>
           Learn more in <a href="/about">here</a>
@@ -25,14 +37,8 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://join.moyu.ooo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Built with by
-          <img src="/moyu.svg" alt="Moyu Logo" className={styles.logo} />
-        </a>
+        Built with by
+        <img src="/moyu.svg" alt="Moyu Logo" className={styles.logo} />
       </footer>
     </div>
   );
